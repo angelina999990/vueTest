e1a8f355d98ec54b6d2cea0bc32b3f8f6636f0d9
xue.zhang <angelina.x.zhang@pwc.com>
2017-12-08 17:44:22 +0800

    update page ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,123 @@
 <template>
-  <h1>hello world</h1>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row>
+      <v-flex md3>
+        <v-expansion-panel class="elevation-0">
+          <v-expansion-panel-content hide-actions v-model="isOpen" class="transparent">
+            <div slot="header" class="deep-purple--text text--darken-4">
+              <span class="mr-2"><v-icon class="deep-purple--text text--darken-4">fa-list</v-icon></span>
+              <span class="title">Application Form</span>
+            </div>
+            <v-stepper vertical class="elevation-0 transparent">
+              <template v-for="(step, index) in steps">
+                <v-stepper-step :step="index+1" :complete="step.isDone">Program Preference</v-stepper-step>
+                <v-stepper-content :step="index+1"></v-stepper-content>
+              </template>
+            </v-stepper>
+          </v-expansion-panel-content>
+          <v-expansion-panel-content hide-actions class="transparent">
+            <div slot="header" class="deep-purple--text text--darken-4">
+              <span class="mr-2">
+                <v-icon class="deep-purple--text text--darken-4">fa-list</v-icon>
+              </span>
+              <span class="title">Application Payment</span>
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-flex>
+      <v-flex md9>
+        <v-card class="pr-3 pl-3">
+          <v-card-title primary-title tile>
+            <h2 class="deep-purple--text text--darken-4 mb-0"><b>PERSON PARTICULARS</b></h2>
+          </v-card-title>
+          <v-card-text class="text-md-left">
+            <v-layout row>
+              <v-flex md2>
+                NAME
+              </v-flex>
+              <v-flex md10>
+                <v-layout class="row wrap" justify-start>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Passport Family Name</div>
+                    <input type="text" class="field-control">
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Passport Given Name</div>
+                    <input type="text" class="field-control">
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                </v-layout>
+                <v-layout class="row wrap mt-4" justify-start>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Alias / Nickname</div>
+                    <input type="text" class="field-control">
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Chinese Name (if any)</div>
+                    <input type="text" class="field-control">
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+
+            <v-divider class="mt-5 mb-5"></v-divider>
+
+            <v-layout row>
+              <v-flex md2>
+                NATIONALITY
+              </v-flex>
+              <v-flex md10>
+                <v-layout class="row wrap" justify-start>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Passport Family Name</div>
+                    <v-select
+                      v-bind:items="items"
+                      v-model="nationality"
+                      label="Select"
+                      single-line
+                      bottom
+                      @input="onInput"
+                    ></v-select>
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Passport Given Name</div>
+                    <input type="text" class="field-control">
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                </v-layout>
+                <v-layout class="row wrap mt-4" justify-start>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Alias / Nickname</div>
+                    <input type="text" class="field-control">
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                  <v-flex md6 class="form-filed">
+                    <div class="field-title">Chinese Name (if any)</div>
+                    <input type="text" class="field-control">
+                    <div class="field-comment">Same as your ID or passport</div>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+
+            <v-divider class="mt-5 mb-5"></v-divider>
+
+            <v-layout>
+              <v-btn outline color="indigo">Previous</v-btn>
+              <v-layout row justify-end class="white--text elevation-0">
+                <v-btn color="indigo" class="white--text">Save</v-btn>
+                <v-btn color="indigo" class="white--text">Save and Next</v-btn>
+              </v-layout>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -7,26 +125,44 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      steps: [
+        {name: 'Program Preference', isDone: true},
+        {name: 'Personal Particulars', isDone: false},
+        {name: 'Contact Infomation', isDone: false}
+      ],
+      isOpen: true,
+      items: ['China', 'USA', 'Australia', 'New Zealand'],
+      nationality: ''
+    }
+  },
+
+  methods: {
+    onInput: function () {
+      console.log(this.nationality)
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.form-filed {
+  .field-title {
+    font-size: 14px;
+    margin-bottom: 4px;
+  }
+
+  .field-comment {
+    font-size: 12px;
+    margin-top: 4px;
+    color: #777;
+  }
+
+  input.field-control {
+    width: 100%;
+    height: 30px;
+    border: 1px solid #311b92;
+    border-radius: 2px;
+  }
 }
 </style>
